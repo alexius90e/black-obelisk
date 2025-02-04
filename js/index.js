c5d77aacdf8ca78314d12766cf3b671e8b4e9171
Aliaksei Karzhou <alexius90e@gmail.com>
2025-02-04 18:04:31 +0300

    feat: added stones

--- js/index.js
+++ js/index.js
@@ -1,16 +1,51 @@
 const examplesCards = document.querySelectorAll('.examples__card');
 
 examplesCards.forEach((card) => {
-  const slider = card.querySelector('.examples__card-slider .swiper');
-  const prevButton = card.querySelector('.examples__card-slider-controls-prev');
-  const nextButton = card.querySelector('.examples__card-slider-controls-next');
+  const sliderEl = card.querySelector('.examples__card-slider .swiper');
+  const prevButtonEl = card.querySelector('.examples__card-slider-controls-prev');
+  const nextButtonEl = card.querySelector('.examples__card-slider-controls-next');
 
-  const swiper = new Swiper(slider, {
+  const swiper = new Swiper(sliderEl, {
     slidesPerView: 1,
     spaceBetween: 20,
     navigation: {
-      nextEl: nextButton,
-      prevEl: prevButton,
+      nextEl: nextButtonEl,
+      prevEl: prevButtonEl,
+    },
+  });
+});
+
+const stonesSliders = document.querySelectorAll('.stones__slider');
+
+stonesSliders.forEach((stonesSlider) => {
+  const sliderEl = stonesSlider.querySelector('.swiper');
+  const prevButtonEl = stonesSlider.querySelector('.stones__slider-controls-prev');
+  const nextButtonEl = stonesSlider.querySelector('.stones__slider-controls-next');
+
+  const swiper = new Swiper(sliderEl, {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 5,
+      },
+      1120: {
+        slidesPerView: 6,
+      },
+    },
+    navigation: {
+      nextEl: nextButtonEl,
+      prevEl: prevButtonEl,
     },
   });
 });
